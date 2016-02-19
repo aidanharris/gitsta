6f35749475dcaa9d41e66ef7d9025c4679de3650
Aidan Harris <mail@aidanharris.io>
2016-02-19 23:27:18 +0000

    Alter sidebar and CSS

--- footer.php
+++ footer.php
@@ -8,14 +8,14 @@
                 </div>
                 <div class="col-md-6 text-right">
                     <small class="text-muted">
-                        <?php printf(__('&copy <strong>%s</strong>', 'gitsta'), get_bloginfo('name')); ?></strong><br>
+                        <?php printf(__('<a href="https://' . $_SERVER['HTTP_HOST'] . '/copyright/">&copy <strong>%s</strong></a>', 'gitsta'), get_bloginfo('name')); ?></strong><br>
                         <?php printf(__('Theme by %s', 'gitsta'), '<a href="' . esc_url('https://github.com/aidanharris/gitsta') . '" target="_blank">Aidan Harris</a>'); ?>
                     </small>
                 </div>
             </div>
         </footer>
     </div>
-    
+
     <?php wp_footer(); ?>
 </body>
 </html>
